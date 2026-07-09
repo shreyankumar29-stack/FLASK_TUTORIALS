27946d0329dc4ace71e416a4a8b362ee279cc0bc
Shreyansh Kumar <shreyan.kumar29@gmail.com>
2026-07-09 17:28:47 +0530

    Modified Flaskblog

--- flaskblog.py
+++ flaskblog.py
@@ -2,12 +2,14 @@ from flask import Flask
 app = Flask(__name__) #setting an instance of flask class
 
 @app.route("/") # shows the information for the routes
-def Hello():    
+@app.route("/home") # shows the information for the routes
+def Home():    
     return "<h1>Home Page</h1>" #returning a string
 
 @app.route("/about") # shows the information for the routes
 def About():    
     return "<h1>About Page</h1>" #returning a string
+
 
 if __name__ == "__main__":
     app.run(debug=True)   #running the app in debug mode
